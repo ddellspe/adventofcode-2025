3fd6532e231fc6eb400403772d563912442f0e8f
David Dellsperger <david.dellsperger@gmail.com>
2025-12-02 03:45:03 -0600

    Day 2 Solution

---
+++ day02/part2.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def is_valid(val: int) -> bool:
+    value = str(val)
+    size = len(value)
+    for pattern_size in range(1, size//2 + 1):
+        if size % pattern_size != 0:
+            continue
+        groups = {
+            value[i:i+pattern_size]
+            for i in range(0, size, pattern_size)
+        }
+        if len(groups) == 1:
+            return False
+    return True
+
+
+def compute(s: str) -> int:
+    invalid_ids = 0
+    ranges = [(int(range.split('-')[0]), int(range.split('-')[1]))
+              for range in s.split(',')]
+    for (start, end) in ranges:
+        for id in range(start, end + 1):
+            if not is_valid(id):
+                invalid_ids += id
+
+    return invalid_ids
+
+
+INPUT_S = '''\
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+'''
+EXPECTED = 4174379265
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
