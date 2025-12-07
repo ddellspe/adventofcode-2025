76e2756505abffe698bfb803f5d5bdc75273bcea
David Dellsperger <david.dellsperger@gmail.com>
2025-12-06 23:40:09 -0600

    Day 7 solution

---
+++ day07/part1.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    splits = 0
+    grid: list[str] = [line for line in s.splitlines()]
+    beams: set[int] = {grid[0].index('S')}
+    for line in grid[1:]:
+        next: set[int] = set()
+        for col in beams:
+            if line[col] == '^':
+                splits += 1
+                next.add(col-1)
+                next.add(col+1)
+            else:
+                next.add(col)
+        beams = next
+    return splits
+
+
+INPUT_S = '''\
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+'''
+EXPECTED = 21
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
