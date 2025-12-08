afbe7f75c0eb74efe5ea2ad4b762e3543300ae66
David Dellsperger <david.dellsperger@gmail.com>
2025-12-08 07:47:38 -0600

    Day 8 solution

---
+++ day08/part2.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+import argparse
+import itertools
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def calculate_distance(
+        box1: tuple[int, int, int],
+        box2: tuple[int, int, int],
+) -> float:
+    return sum([(box2[i] - box1[i])**2 for i in range(3)])
+
+
+def compute(s: str) -> int:
+    boxes = set()
+    for line in s.splitlines():
+        x, y, z = line.split(',')
+        boxes.add((int(x), int(y), int(z)))
+    connections = list(itertools.combinations(boxes, 2))
+    connections.sort(key=lambda x: calculate_distance(x[0], x[1]))
+
+    circuit_leaders = {box: box for box in boxes}
+    leader_count = len(boxes)
+
+    def get_leader(
+            box: tuple[int, int, int],
+            leaders: dict[tuple[int, int, int], tuple[int, int, int]],
+    ) -> tuple[int, int, int]:
+        if leaders[box] != box:
+            leaders[box] = get_leader(leaders[box], leaders)
+        return leaders[box]
+
+    for box1, box2 in connections:
+        leader1 = get_leader(box1, circuit_leaders)
+        leader2 = get_leader(box2, circuit_leaders)
+        if leader1 != leader2:
+            circuit_leaders[leader1] = leader2
+            leader_count -= 1
+        if leader_count == 1:
+            return box1[0] * box2[0]
+    return 0
+
+
+INPUT_S = '''\
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+'''
+EXPECTED = 25272
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
