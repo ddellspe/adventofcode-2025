2a7e1d7d0838708da653e658a109f484f65b21fb
David Dellsperger <david.dellsperger@gmail.com>
2025-12-12 08:08:12 -0600

    Day 12 solution

---
+++ day12/part1.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+import re
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    gifts = []
+    *gift_xrays, areas = s.split('\n\n')
+    for gift in gift_xrays:
+        gifts.append(''.join(gift.splitlines()[1:]).count('#'))
+
+    fits = 0
+    for area in areas.splitlines():
+        width, length, *counts = (int(x) for x in re.split(r'\D+', area))
+        size = sum(count * box for count, box in zip(counts, gifts))
+        if size < width * length:
+            fits += 1
+
+    return fits
+
+
+INPUT_S = '''\
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
+'''
+EXPECTED = 2
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
